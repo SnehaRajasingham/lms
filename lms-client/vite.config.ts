094d42139c6c4cad763b08a32c441add8f4f2ead
sneha <rajasinghamsneha@gmai.com>
2025-05-01 10:50:15 +0530

    chore: add setupTests file for jest-dom and update vite config

--- lms-client/vite.config.ts
+++ lms-client/vite.config.ts
@@ -1,12 +1,14 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-// https://vite.dev/config/
+import { configDefaults } from 'vitest/config';
+
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
+    setupFiles: './src/setupTests.ts',
+    exclude: [...configDefaults.exclude],
   },
 });
